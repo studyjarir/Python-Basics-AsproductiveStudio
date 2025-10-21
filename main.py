9e0560fdd278fb44ec8bd772fa66c77b81401706
jarir <studyjarir@gmail.com>
2025-10-21 23:36:54 +0600

    new code name added

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Developed, Authorized & Owned by ...")
+print("") #Space!
+print("") #Space!
+
+print("|||||||| ||||||||                    ")
+print("||    || ||                          ")
+print("|||||||| ||||||||                    ")
+print("||    ||       ||                    ")
+print("||    || |||||||| PRODUCTIVE STUDIO  ")
+print("") #Space!
+
+print("|||||||||||||||||||||||||||||||||||")
+print("") #Space!
+
+print("Insert the corresponding number for executing the code. ")
+print("") #Space!
+
+print("1. Creating a program to take two numbers as input and print their sum. ")
