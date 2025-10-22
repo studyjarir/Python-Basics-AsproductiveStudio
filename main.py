5cd77639b6f07242b244e53d1f00834962ac94d9
jarir <studyjarir@gmail.com>
2025-10-22 23:38:27 +0600

    Found the restart prob

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
-print("Developed, Authorized & Owned by ...")
-print("") #Space!
-print("") #Space!
+def main():
 
-print("|||||||| ||||||||                    ")
-print("||    || ||                          ")
-print("|||||||| ||||||||                    ")
-print("||    ||       ||                    ")
-print("||    || |||||||| PRODUCTIVE STUDIO  ")
-print("") #Space!
+    print("Developed, Authorized & Owned by ...")
+    print("") #Space!
+    print("") #Space!
 
-print("|||||||||||||||||||||||||||||||||||")
-print("") #Space!
+    print("|||||||| ||||||||                    ")
+    print("||    || ||                          ")
+    print("|||||||| ||||||||                    ")
+    print("||    ||       ||                    ")
+    print("||    || |||||||| PRODUCTIVE STUDIO  ")
+    print("") #Space!
 
-print("Insert the corresponding number for executing the code. ")
-print("") #Space!
+    print("===================================")
+    print("") #Space!
 
-print("1. Creating a program to take two numbers as input and print their sum. ")
+    print("Insert the corresponding number for executing the code. ")
+    print("") #Space!
+
+    print(" 1. Creating a program to take two numbers as input and print their sum. ")
+    print(" 2. Creating a program for indetify if the student is passed or failed. ")
+    print(" 3. Creating a program that checks if a citizen is eligible to vote based on their age. ")
+    print(" 4. Creating a program to determine if a number is even or odd. ")
+    print(" 5. Creating a program whether the given year is leap year or not. ")
+    print(" 6. ")
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
